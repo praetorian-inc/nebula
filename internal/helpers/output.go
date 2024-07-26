84caab03910851e544031186c71f0e22d396cd3a
Zach Grace <zach.grace@praetorian.com>
2024-07-26 10:44:26 -0500

    adding output and logging updates

---
+++ internal/helpers/output.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+
+	l "github.com/praetorian-inc/nebula/internal/logs"
+	"github.com/praetorian-inc/nebula/modules"
+)
+
+func PrintMessage(message string) {
+	fmt.Println(message)
+	logger := l.ConsoleLogger()
+	logger.Info(message)
+}
+
+func PrintResult(result modules.Result) {
+	logger := l.ConsoleLogger()
+	r, _ := json.MarshalIndent(result.Data, "", "  ")
+	if len(r) > 250 {
+		logger.Info("Summary", "result", string(r[:100]))
+	} else {
+		logger.Info("Result", "result", string(r))
+	}
+
+}

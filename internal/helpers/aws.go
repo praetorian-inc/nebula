c85a6a9162cb14de6250db0460307bc2a8db6347
Zach Grace <zach.grace@praetorian.com>
2024-08-02 09:06:45 -0500

    adding missing function

--- internal/helpers/aws.go
+++ internal/helpers/aws.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/praetorian-inc/nebula/internal/logs"
 )
 
@@ -27,4 +28,16 @@ func GetAWSCfg(region string, profile string) (aws.Config, error) {
 	}
 
 	return cfg, nil
+}
+
+func GetAccountId(cfg aws.Config) (string, error) {
+	client := sts.NewFromConfig(cfg)
+	input := &sts.GetCallerIdentityInput{}
+
+	result, err := client.GetCallerIdentity(context.TODO(), input)
+	if err != nil {
+		return "", err
+	}
+
+	return *result.Account, nil
 }

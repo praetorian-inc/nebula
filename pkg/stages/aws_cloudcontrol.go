efa7394a0e8de28302ebdc11440a9f3b3f294037
xoverride <13634432+xoverride@users.noreply.github.com>
2025-02-12 11:39:27 -0500

    Update aws_cloudcontrol.go
    Refactor the ListResources func

--- pkg/stages/aws_cloudcontrol.go
+++ pkg/stages/aws_cloudcontrol.go
@@ -2,13 +2,14 @@ package stages
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/cloudcontrol/types"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudcontrol"
-	cctypes "github.com/aws/aws-sdk-go-v2/service/cloudcontrol/types"
 	"github.com/praetorian-inc/nebula/internal/helpers"
 	"github.com/praetorian-inc/nebula/internal/logs"
 	"github.com/praetorian-inc/nebula/modules/options"
@@ -19,72 +20,99 @@ import (
 // This should be the primary method of listing resources in AWS.
 func AwsCloudControlListResources(ctx context.Context, opts []*types.Option, rtype <-chan string) <-chan types.EnrichedResourceDescription {
 	logger := logs.NewStageLogger(ctx, opts, "CloudControlListResources")
+	out := make(chan types.EnrichedResourceDescription, 100) // Buffered to reduce blocking
 
-	out := make(chan types.EnrichedResourceDescription)
 	logger.Info("Listing resources")
 
 	profile := options.GetOptionByName("profile", opts).Value
 	regions, err := helpers.ParseRegionsOption(options.GetOptionByName(options.AwsRegionsOpt.Name, opts).Value, profile, opts)
 	if err != nil {
-		logger.Error(err.Error())
+		close(out)
 		return nil
 	}
 
 	config, err := helpers.GetAWSCfg(regions[0], profile, opts)
 	if err != nil {
 		logger.Error(err.Error())
+		close(out)
 		return nil
 	}
+
 	acctId, err := helpers.GetAccountId(config)
 	if err != nil {
 		logger.Error(err.Error())
+		close(out)
 		return nil
+	}
+
+	// Preload AWS clients
+	cloudControlClients := make(map[string]*cloudcontrol.Client)
+	regionSemaphores := make(map[string]chan struct{})
+
+	for _, region := range regions {
+		config, err := helpers.GetAWSCfg(region, profile, opts)
+		if err != nil {
+			logger.Error("Failed to get AWS config for", "region", region, "err:", err)
+			continue
+		}
+
+		cloudControlClients[region] = cloudcontrol.NewFromConfig(config)
+		regionSemaphores[region] = make(chan struct{}, 5) // Limit concurrency per region
 	}
 
 	var wg sync.WaitGroup
 
-	// Create semaphores for each region to limit concurrent resource processing
-	regionSemaphores := make(map[string]chan struct{})
-	for _, region := range regions {
-		regionSemaphores[region] = make(chan struct{}, 5)
-	}
-
 	for rtype := range rtype {
-		for _, region := range regions {
-			// Skip non us-east-1 regions for global services
-			if helpers.IsGlobalService(rtype) && region != "us-east-1" {
+		for region, cc := range cloudControlClients {
+			if cc == nil {
+				logger.Debug("Missing CloudControl Client for region", region)
 				continue
 			}
 
-			logger.Info("Listing resources of type " + rtype + " in region: " + region)
-			wg.Add(1)
-			go func(region string, rtype string) {
-				defer wg.Done()
-				config, _ := helpers.GetAWSCfg(region, profile, opts)
-				cc := cloudcontrol.NewFromConfig(config)
-				params := &cloudcontrol.ListResourcesInput{
-					TypeName: &rtype,
-				}
+			if helpers.IsGlobalService(rtype) && region != "us-east-1" {
+				logger.Debug("Skipping global resource type %s in region %s", rtype, region)
+				continue
+			}
 
-				for {
-					res, err := cc.ListResources(ctx, params)
+			logger.Debug("Listing resources of type %s in region: %s", rtype, region)
+			wg.Add(1)
+
+			go func(region, rtype string) {
+				defer wg.Done()
+				cc := cloudControlClients[region]
+
+				paginator := cloudcontrol.NewListResourcesPaginator(cc, &cloudcontrol.ListResourcesInput{
+					TypeName:   &rtype,
+					MaxResults: aws.Int32(100),
+				})
+
+				for paginator.HasMorePages() {
+					select {
+					case <-ctx.Done():
+						logger.Info("Context cancelled, stopping pagination for", rtype, "in region", region)
+						break
+					case regionSemaphores[region] <- struct{}{}: // Acquire semaphore
+					}
+
+					res, err := paginator.NextPage(ctx)
 					if err != nil {
+						<-regionSemaphores[region] // Release semaphore
 						if strings.Contains(err.Error(), "TypeNotFoundException") {
-							logger.Info("The type %s is not available in region %s", rtype, region)
-							break
+							logger.Debug("The type %s is not available in region %s", rtype, region)
+						} else {
+							logger.Debug("Failed to ListResources", "region", region, "type", rtype, "err", err.Error())
 						}
-						logger.Debug(err.Error())
 						break
 					}
+
+					<-regionSemaphores[region] // Release semaphore
 
 					var resourceWg sync.WaitGroup
 					for _, resource := range res.ResourceDescriptions {
 						resourceWg.Add(1)
-						go func(resource *cctypes.ResourceDescription) {
-							defer resourceWg.Done()
-							regionSemaphores[region] <- struct{}{}
-							defer func() { <-regionSemaphores[region] }()
 
+						go func(resource awstypes.ResourceDescription) {
+							defer resourceWg.Done()
 							erd := types.EnrichedResourceDescription{
 								Identifier: *resource.Identifier,
 								TypeName:   rtype,
@@ -93,16 +121,17 @@ func AwsCloudControlListResources(ctx context.Context, opts []*types.Option, rty
 								AccountId:  acctId,
 							}
 							erd.Arn = erd.ToArn()
-							out <- erd
-						}(&resource)
+
+							select {
+							case out <- erd:
+							case <-ctx.Done(): // Handle context cancellation
+								return
+							}
+						}(resource)
 					}
 					resourceWg.Wait()
-
-					if res.NextToken == nil {
-						break
-					}
-					params.NextToken = res.NextToken
 				}
+
 				logger.Info("Completed collecting resource type " + rtype + " in region: " + region)
 			}(region, rtype)
 		}
@@ -111,6 +140,9 @@ func AwsCloudControlListResources(ctx context.Context, opts []*types.Option, rty
 	go func() {
 		wg.Wait()
 		close(out)
+		for _, ch := range regionSemaphores {
+			close(ch)
+		}
 	}()
 
 	return out

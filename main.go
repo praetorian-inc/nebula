16bbff6a8f6e87be137dfe7f6f65af096831c336
josephwhenry <joseph.henry@praetorian.com>
2025-03-10 17:23:26 -0500

    mod bump

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"log/slog"
-	"os"
 	"runtime/debug"
 
 	"github.com/praetorian-inc/nebula/cmd"
 )
 
 func main() {
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
-	slog.SetDefault(logger)
-
 	debug.SetMaxThreads(20000)
 	cmd.Execute()
 }

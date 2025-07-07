ba694ee649a1e7b7963a5fc674db50b76dc01787
Zach Grace <zach.grace@praetorian.com>
2025-07-07 15:32:32 -0500

    adding aws-public-resources-single

---
+++ pkg/modules/aws/recon/public_resources_single.go
@@ -0,0 +1,31 @@
+package recon
+
+import (
+	"github.com/praetorian-inc/janus/pkg/chain"
+	"github.com/praetorian-inc/janus/pkg/chain/cfg"
+	"github.com/praetorian-inc/janus/pkg/output"
+	"github.com/praetorian-inc/nebula/pkg/links/aws"
+	"github.com/praetorian-inc/nebula/pkg/links/general"
+	"github.com/praetorian-inc/nebula/pkg/links/options"
+	"github.com/praetorian-inc/nebula/pkg/outputters"
+)
+
+var AWSPublicResourcesSingle = chain.NewModule(
+	cfg.NewMetadata(
+		"AWS Public Resources Single",
+		"Enumerate public AWS resources",
+	).WithProperties(map[string]any{
+		"id":          "public-resources-single",
+		"platform":    "aws",
+		"opsec_level": "moderate",
+		"authors":     []string{"Praetorian"},
+	}).
+		WithChainInputParam(options.AwsResourceArn().Name()),
+).WithLinks(
+	general.NewSingleResourcePreprocessor(),
+	aws.NewAwsPublicResources,
+).WithOutputters(
+	output.NewJSONOutputter,
+	//output.NewConsoleOutputter,
+	outputters.NewERDConsoleOutputter,
+)

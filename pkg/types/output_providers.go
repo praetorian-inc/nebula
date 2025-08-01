ec069e2fa886f7d977742ba43d2b5b829e4675b0
Zach Grace <zach.grace@praetorian.com>
2025-07-31 22:07:26 -0500

    removing unwanted changes

--- pkg/types/output_providers.go
+++ pkg/types/output_providers.go
@@ -1,7 +1,5 @@
 package types
 
-import "strings"
-
 type OutputProvider interface {
 	Write(result Result) error
 }
@@ -12,42 +10,4 @@ type MarkdownTable struct {
 	TableHeading string
 	Headers      []string
 	Rows         [][]string
-}
-
-func (t *MarkdownTable) ToMarkdown() string {
-	var builder strings.Builder
-
-	if t.TableHeading != "" {
-		builder.WriteString(t.TableHeading)
-		builder.WriteString("\n\n")
-	}
-
-	// Write headers
-	builder.WriteString("|")
-	for _, header := range t.Headers {
-		builder.WriteString(" ")
-		builder.WriteString(header)
-		builder.WriteString(" |")
-	}
-	builder.WriteString("\n")
-
-	// Write separator
-	builder.WriteString("|")
-	for range t.Headers {
-		builder.WriteString("---|")
-	}
-	builder.WriteString("\n")
-
-	// Write rows
-	for _, row := range t.Rows {
-		builder.WriteString("|")
-		for _, cell := range row {
-			builder.WriteString(" ")
-			builder.WriteString(cell)
-			builder.WriteString(" |")
-		}
-		builder.WriteString("\n")
-	}
-
-	return builder.String()
 }
